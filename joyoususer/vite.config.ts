9df63b397c18534a1282dea08eff586017db6daf
JoyousUser <samijenaouniboite@gmail.com>
2025-07-31 12:43:42 +0200

    test

--- joyoususer/vite.config.ts
+++ joyoususer/vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({  plugins: [    tailwindcss(),  react() ],  base: '/joyoususer.github.io/', })
+export default defineConfig({  plugins: [    tailwindcss(),  react() ],  base: '/', })
